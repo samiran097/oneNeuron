01968946ee7a91ca936c9f2d0754a85508206a7e
Samiran Adak <samiran.adak@teamhgs.com>
2021-09-28 12:03:02 +0530

    first commit

--- and.py
+++ and.py
@@ -1,5 +1,7 @@
 from utils.model import Perceptron
-from utils.all_utils import prepare_data
+from utils.all_utils import prepare_data,save_model,save_plot
+import pandas as pd
+
 
 AND = {
     "x1": [0,0,1,1],
@@ -9,7 +11,7 @@ AND = {
 
 df = pd.DataFrame(AND)
 
-df
+print(df)
 
 X,y = prepare_data(df)
 
@@ -20,3 +22,6 @@ model = Perceptron(eta=ETA, epochs=EPOCHS)
 model.fit(X, y)
 
 _ = model.total_loss()
+
+save_model(model,filename="and.model")
+save_plot(df,"and.png",model)
